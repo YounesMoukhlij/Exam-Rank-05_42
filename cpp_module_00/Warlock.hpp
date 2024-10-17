590b69a119b4845cae25a4a95ea4ac3087d4f394
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-17 14:26:40 +0100

    by gacp

---
+++ cpp_module_00/Warlock.hpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Warlock.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/17 14:20:11 by youmoukh          #+#    #+#             */
+/*   Updated: 2024/10/17 14:24:22 by youmoukh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <iostream>
+
+class Warlock
+{
+	private:
+		Warlock();
+		Warlock(const Warlock& origine);
+		Warlock& operator=(const Warlock& origine);
+		std::string	name;
+		std::string	title;
+	public:
+		Warlock(std:string& _name, std::string& _title);
+		~Warlock();
+
+		std::string const	&getName() const;
+		std::string	const 	&getTitle() const;
+		void	introduce() const;
+};
