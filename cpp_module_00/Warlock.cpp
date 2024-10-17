25015d4806562f009276081fb74d6c178aee1e6c
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-17 14:44:03 +0100

    by gacp

--- cpp_module_00/Warlock.cpp
+++ cpp_module_00/Warlock.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 14:20:09 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/17 14:43:50 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/17 14:44:00 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,4 +45,4 @@ Warlock& Warlock::operator=(const Warlock& origine)
 	return (*this);
 }
 
-Warlock
+vWarlock::setTitle()
