9d6002da9275d73d6ffa1f1a0449c4d79d7ec95a
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-17 15:06:50 +0100

    by gacp

--- cpp_module_01/main.cpp
+++ cpp_module_01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 14:19:27 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/17 15:06:43 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/17 15:06:48 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,17 +18,17 @@
 
 int main()
 {
-  Warlock richard("Richard", "the Titled");
-
-  Dummy bob;
-  Fwoosh* fwoosh = new Fwoosh();
-
-  richard.learnSpell(fwoosh);
-
-  richard.introduce();
-  richard.launchSpell("Fwoosh", bob);
-
-  richard.forgetSpell("Fwoosh");
-  richard.launchSpell("Fwoosh", bob);
-eturn (0);
+  	Warlock richard("Richard", "the Titled");
+	
+  	Dummy bob;
+  	Fwoosh* fwoosh = new Fwoosh();
+	
+  	richard.learnSpell(fwoosh);
+	
+  	richard.introduce();
+  	richard.launchSpell("Fwoosh", bob);
+	
+  	richard.forgetSpell("Fwoosh");
+  	richard.launchSpell("Fwoosh", bob);
+	return (0);
 }
