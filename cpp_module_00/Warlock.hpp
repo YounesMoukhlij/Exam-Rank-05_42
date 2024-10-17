01122f17509470437beff8ac6461d777bf333120
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-17 14:43:37 +0100

    by gacp

--- cpp_module_00/Warlock.hpp
+++ cpp_module_00/Warlock.hpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 14:20:11 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/17 14:38:27 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/17 14:43:36 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,4 +30,5 @@ class Warlock
 		const std::string&    	getName() const;
 		const std::string&	 	getTitle() const;
 		void					introduce() const;
+		setTitle
 };
