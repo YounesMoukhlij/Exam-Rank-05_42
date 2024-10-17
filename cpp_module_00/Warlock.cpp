fe9466f34cb2ebf5186ba6850a7f16fde8424215
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-17 14:30:21 +0100

    by gacp

--- cpp_module_00/Warlock.cpp
+++ cpp_module_00/Warlock.cpp
@@ -6,13 +6,16 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 14:20:09 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/17 14:30:12 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/17 14:30:17 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Warlock.hpp"
 
-Warlock::Warlock(std:string& _name, std::string& _title) {}
+Warlock::Warlock(std:string& _name, std::string& _title)
+{
+	
+}
 
 Warlock::~Warlock() {}
 
