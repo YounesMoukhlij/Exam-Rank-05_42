a95516604b9432a377108e8ac09903eda6559c07
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-17 14:40:48 +0100

    by gacp

--- cpp_module_00/Warlock.cpp
+++ cpp_module_00/Warlock.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 14:20:09 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/17 14:40:37 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/17 14:40:47 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,5 +40,6 @@ void	Warlock::introduce() const
 
 Warlock::operator=(const Warlock& origine)
 {
+	
 	
 }
