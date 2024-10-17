071277e91bbe4c23c0552baf056a0ec92c1e3ed6
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-17 14:49:09 +0100

    by gacp

--- cpp_module_00/Warlock.cpp
+++ cpp_module_00/Warlock.cpp
@@ -6,13 +6,13 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 14:20:09 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/17 14:49:04 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/17 14:49:07 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Warlock.hpp"
 
-Warlock::Warlock(const std::string& _name, std::string& _title) : name(_name), title(_title)
+Warlock::Warlock(const std::string& _name,std::string& _title) : name(_name), title(_title)
 {
 	std::cout << _name << " : This looks like another boring day." << std::endl;
 }
