7bd04c121bd138ded4d0a8e0261943629127e0e6
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-17 14:42:52 +0100

    by gacp

--- cpp_module_00/Warlock.cpp
+++ cpp_module_00/Warlock.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 14:20:09 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/17 14:41:26 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/17 14:42:50 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,7 +38,7 @@ void	Warlock::introduce() const
 	
 }
 
-Warlock::operator=(const Warlock& origine)
+Wa Warlock::operator=(const Warlock& origine)
 {
 	this->name = origine.name;
 	this->title = origine.title;
