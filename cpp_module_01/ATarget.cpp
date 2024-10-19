74a81a78fb57229cd4e0b33759faae04bc9e73b6
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-19 13:31:38 +0100

    by gacp

--- cpp_module_01/ATarget.cpp
+++ cpp_module_01/ATarget.cpp
@@ -6,7 +6,39 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/19 13:03:35 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/19 13:03:35 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/19 13:31:31 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ATarget.hpp"
+
+ATarget::ATarget(std::string type) : _type(type)
+{
+
+}
+
+ATarget & ATarget::operator=(ATarget const & rhs)
+{
+	_type = rhs.getType();
+	return *this;
+}
+
+ATarget::ATarget(ATarget const & obj)
+{
+	*this = obj;
+}
+
+ATarget::~ATarget()
+{
+
+}
+
+std::string ATarget::getType() const
+{
+	return (_type);
+}
+
+void	ATarget::getHitBySpell(ASpell const & spell) const
+{
+	std::cout << _type << " has been " << spell.getEffects() << "!" << std::endl;
+}
