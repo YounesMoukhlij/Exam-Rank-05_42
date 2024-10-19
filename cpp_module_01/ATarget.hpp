7d8bc3e0fec52b807c68bc26b60a52cb8909fce7
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-19 13:14:42 +0100

    by gacp

--- cpp_module_01/ATarget.hpp
+++ cpp_module_01/ATarget.hpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/19 13:03:38 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/19 13:14:29 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/19 13:14:37 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,19 @@
 
 #include <iostream>
 
-class ASpell
+class ATarget
 {
 	private:
 		std::string name;
 		std::string effect;
 	public:
-		ASpell(const ASpell& origine);
-		ASpell();
-		ASpell(std::string _name, std:string _effects);
-		ASpell& operator=(const ASpell& origine);
-		virtual ~ASpell();
+		ATarget(const ATarget& origine);
+		ATarget();
+		ATarget(std::string _name, std:string _effects);
+		ATarget& operator=(const ATarget& origine);
+		virtual ~ATarget();
 		const std::string getName() const;
 		const std::string getEffects() const;
-		virtual ASpell*  clone() = 0;
+		virtual ATarget*  clone() = 0;
 		
 };
