287013c4f501e9891b9e11e28f2ffc81778a306e
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-19 13:28:08 +0100

    by gacp

--- cpp_module_01/Fwoosh.hpp
+++ cpp_module_01/Fwoosh.hpp
@@ -6,7 +6,16 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/19 13:04:04 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/19 13:04:04 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/19 13:28:05 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+
+
+class Fwoosh : public ASpell
+{
+	public :
+		Fwoosh();
+		~Fwoosh();
+		ASpell* clone() const;
+};
