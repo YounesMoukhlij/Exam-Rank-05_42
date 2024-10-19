c7426e8694ceaf22befe8693137807cf44ddffac
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-19 14:14:39 +0100

    by gacp

--- cpp_module_01/Dummy.hpp
+++ cpp_module_01/Dummy.hpp
@@ -6,14 +6,14 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/19 13:04:21 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/19 14:14:17 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/19 14:14:22 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # pragma once
 
 #include "ASpell.hpp"
-#include "ASpell.hpp"
+#include "ATarget.hpp"
 
 class Dummy : public ASpell
 {
