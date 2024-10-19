5b66e8b1740978be866ab0501ac269122813507e
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-19 13:17:15 +0100

    by gacp

--- cpp_module_01/ATarget.hpp
+++ cpp_module_01/ATarget.hpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/19 13:03:38 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/19 13:16:57 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/19 13:17:14 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ class ATarget
 	public:
 		ATarget(const ATarget& origine);
 		ATarget();
-		ATarget(std::string _name, std:string _effects);
+		ATarget(std::string type);
 		ATarget& operator=(const ATarget& origine);
 		virtual ~ATarget();
 		const std::string getName() const;
