c096d607ad5f207dbe9a1bfcd3770c255688e7df
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-19 13:17:24 +0100

    by gacp

--- cpp_module_01/ATarget.hpp
+++ cpp_module_01/ATarget.hpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/19 13:03:38 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/19 13:17:14 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/19 13:17:23 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ class ATarget
 		ATarget(std::string type);
 		ATarget& operator=(const ATarget& origine);
 		virtual ~ATarget();
-		const std::string getName() const;
+		const std::string getType() const;
 		const std::string getEffects() const;
 		virtual ATarget*  clone() = 0;
 		void getHitBySpell(const ASpell& aspell);
