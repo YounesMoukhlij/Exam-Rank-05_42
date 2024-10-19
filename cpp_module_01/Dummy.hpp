87eeb801acf48ec199cf666ae5de0962be2b6ceb
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-19 14:14:19 +0100

    by gacp

--- cpp_module_01/Dummy.hpp
+++ cpp_module_01/Dummy.hpp
@@ -6,12 +6,13 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/19 13:04:21 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/19 14:07:41 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/19 14:14:17 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # pragma once
 
+#include "ASpell.hpp"
 #include "ASpell.hpp"
 
 class Dummy : public ASpell
