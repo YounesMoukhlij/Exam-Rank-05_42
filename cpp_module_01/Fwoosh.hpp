7eb48e0eaf8b0d6d18d6c33291d4f5748d0793b6
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-19 13:28:44 +0100

    by gacp

--- cpp_module_01/Fwoosh.hpp
+++ cpp_module_01/Fwoosh.hpp
@@ -6,13 +6,13 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/19 13:04:04 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/19 13:28:36 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/19 13:28:39 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # pragma once
 
-#inclu
+#include "ASpell.hpp"
 
 class Fwoosh : public ASpell
 {
