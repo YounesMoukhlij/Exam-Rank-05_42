f2ba428d98784bd8d898ab7cc42e172a6e2e7df8
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-19 13:14:32 +0100

    by gacp

--- cpp_module_01/ATarget.hpp
+++ cpp_module_01/ATarget.hpp
@@ -6,8 +6,27 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/19 13:03:38 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/19 13:14:23 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/19 13:14:29 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#pragma once
 
+#include <iostream>
+
+class ASpell
+{
+	private:
+		std::string name;
+		std::string effect;
+	public:
+		ASpell(const ASpell& origine);
+		ASpell();
+		ASpell(std::string _name, std:string _effects);
+		ASpell& operator=(const ASpell& origine);
+		virtual ~ASpell();
+		const std::string getName() const;
+		const std::string getEffects() const;
+		virtual ASpell*  clone() = 0;
+		
+};
