469e2c59ce6cf756d8b25c0805156ce4d204d83e
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-19 14:07:29 +0100

    by gacp

--- cpp_module_01/Dummy.cpp
+++ cpp_module_01/Dummy.cpp
@@ -6,7 +6,20 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/19 13:04:18 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/19 13:04:18 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/19 14:07:25 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Dummy.hpp"
+
+Dummy::Dummy() : ATarget("Target Practice Dummy")
+{
+}
+
+Dummy::~Dummy()
+{}
+
+ATarget* Dummy::clone() const
+{
+	return (new Dummy());
+}
