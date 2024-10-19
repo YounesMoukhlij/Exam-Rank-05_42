d2fe65eb6ef5a8566032e0974007207053897714
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-19 15:26:24 +0100

    by gacp

--- cpp_module_01/Warlock.hpp
+++ cpp_module_01/Warlock.hpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 14:20:11 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/19 15:24:30 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/19 15:26:18 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,13 +22,13 @@ class ASpell;
 class Warlock
 {
 	private:
-		Warlock();
-		Warlock(const Warlock& origine);
-		Warlock& operator=(const Warlock& origine);
 		std::map < std::string, ASpell * > Opr;
 		std::string	name;
 		std::string	title;
 	public:
+		Warlock();
+		Warlock(const Warlock& origine);
+		Warlock& operator=(const Warlock& origine);
 		Warlock(const std::string& _name, const std::string&  _title);
 		~Warlock();
 
