d8614cda3eca8bf24bc10ff439c4741544e24f0d
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-19 14:13:24 +0100

    by gacp

--- cpp_module_01/ASpell.hpp
+++ cpp_module_01/ASpell.hpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 15:48:26 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/19 14:13:10 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/19 14:13:22 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,11 +24,12 @@ class ASpell
 		ASpell(std::string _name, std::string _effects);
 		ASpell& operator=(const ASpell& origine);
 		virtual ~ASpell();
+		
 		std::string getName() const;
 		std::string getEffects() const;
+		void 		launch(ATarget const & target) const;
+		
+		
 		virtual ASpell*  clone() = 0;
-		
-		void launch(ATarget const & target) const;
-		
 };
 
