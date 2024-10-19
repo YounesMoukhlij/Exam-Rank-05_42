253c4385667b4eb7edcbc875742527c65171799c
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-19 13:32:43 +0100

    by gacp

--- cpp_module_01/ASpell.cpp
+++ cpp_module_01/ASpell.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 15:48:29 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/19 13:32:29 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/19 13:32:36 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,4 +20,7 @@ ASpell::~ASpell()
 {
 }
 
-		ASpell(std::string _name, std:string _effects);
+ASpell::ASpell(std::string _name, std:string _effects)
+{
+	
+}
