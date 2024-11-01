e565cff731dc194b9cea0282750b8594eb7d489f
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-11-01 15:34:19 +0100

    by gacp

--- TEST/cpp_module01/Warlock.hpp
+++ TEST/cpp_module01/Warlock.hpp
@@ -59,17 +59,8 @@ void	Warlock::launchSpell(std::string spell, ATarget& const target)
 {
 	if (Opr.find(spell) != Opr.end())
 	{
-		Opr[spell]->launch()
+		Opr[spell]->launch(target);
 	}
 }
 
 
-* learnSpell, takes a pointer to ASpell, that makes the Warlock learn a spell
-* forgetSpell, takes a string corresponding a to a spell's name, and makes the
-  Warlock forget it. If it's not a known spell, does nothing.
-* launchSpell, takes a string (a spell name) and a reference to ATarget, that
-  launches the spell on the selected target. If it's not a known spell, does
-  nothing.
-
-You will need a new attribute to store the spells your Warlock knows. Several
-types fit the bill, it's up to you to choose the best one.
