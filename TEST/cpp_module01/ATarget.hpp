c4f837ed21b5271828ed4260960e5a9c3c86964d
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-11-01 14:49:07 +0100

    by gacp

--- TEST/cpp_module01/ATarget.hpp
+++ TEST/cpp_module01/ATarget.hpp
@@ -25,3 +25,34 @@ void ATarget::getHitBySpell(const ASpell aspell)
 	std::cout << _type << " has been " << aspell.getEffets() << "!" << std::endl;
 }
 
+
+ASpell::ASpell(const std::string& name, const std::string& effects) : _name(name), _effects(effects)
+{
+
+}
+
+ASpell::~ASpell()
+{
+}
+
+
+		ASpell&  ASpell::operator=(const ASpell& origine)
+		{
+			if (this != &origine)
+			{
+				_effects = origine._effects;
+				_name = origine._name;
+			}
+			return (*this);
+		}
+
+
+
+const std::string& ASpell::getName() const
+{
+	return _name;
+}
+const std::string& ASpell::getEffects() const
+{
+	return _effects;
+}
