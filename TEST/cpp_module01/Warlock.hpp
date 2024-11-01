21b2d5edc7250d7bc0228f94e2038b3cd6634e6f
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-11-01 15:14:45 +0100

    by gacp

--- TEST/cpp_module01/Warlock.hpp
+++ TEST/cpp_module01/Warlock.hpp
@@ -19,6 +19,19 @@ class Warlock
 		void introduce() const;
 
 		void	setTitle(const std::string& title);
+		
 
 };
 
+
+
+
+* learnSpell, takes a pointer to ASpell, that makes the Warlock learn a spell
+* forgetSpell, takes a string corresponding a to a spell's name, and makes the
+  Warlock forget it. If it's not a known spell, does nothing.
+* launchSpell, takes a string (a spell name) and a reference to ATarget, that
+  launches the spell on the selected target. If it's not a known spell, does
+  nothing.
+
+You will need a new attribute to store the spells your Warlock knows. Several
+types fit the bill, it's up to you to choose the best one.
