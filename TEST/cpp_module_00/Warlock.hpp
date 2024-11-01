0f4601fc9419db294929c4597c9e7862794bf4d0
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-11-01 14:03:13 +0100

    by gacp

--- TEST/cpp_module_00/Warlock.hpp
+++ TEST/cpp_module_00/Warlock.hpp
@@ -33,5 +33,5 @@ Warlock::~Warlock()
 
 void Warlock::introduce() const
 {
-	std::cout << <NAME>: I am _name << ", "  _title << std::endl;
+	std::cout << _": I am "_name << ", "  _title << std::endl;
 }
