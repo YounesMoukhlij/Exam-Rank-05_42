82e9bee0d56bcf581ab0f2af20bc4003aa9651e8
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-11-01 14:57:19 +0100

    by gacp

--- TEST/cpp_module01/Fwoosh.hpp
+++ TEST/cpp_module01/Fwoosh.hpp
@@ -7,19 +7,11 @@
 
 class Fwoosh
 {
-	protected:
-		std::string _name;
-		std::string _effects;
 
 	public:
-		Fwoosh& operator=(const Fwoosh& origine);
 		Fwoosh();
 		~Fwoosh();
 
-
-		virtual Fwoosh* clone() = 0;
-
-		void	launch(const ATarget& target);
 
 };
 
