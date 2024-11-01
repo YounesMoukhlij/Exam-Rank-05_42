afd909d15f11fde6bb3907fce57dbcfe87bc3986
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-11-01 14:22:28 +0100

    by gacp

--- TEST/cpp_module01/ASpell.cpp
+++ TEST/cpp_module01/ASpell.cpp
@@ -4,12 +4,11 @@
 
 ASpell::ASpell(const std::string& name, const std::string& effects) : _name(name), _effects(effects)
 {
-	std::cout << _name << ": This looks like another boring day." << std::endl;
+
 }
 
 ASpell::~ASpell()
 {
-
 }
 
 
