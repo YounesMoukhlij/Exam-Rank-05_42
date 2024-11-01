84bc419533b13415e77cf4cbbb1c7bf82e4caf47
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-11-01 14:22:57 +0100

    by gacp

---
+++ TEST/cpp_module01/ATarget.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <iostream>
+
+
+class ASpell
+{
+	protected:
+		std::string _name;
+		std::string _effects;
+
+	public:
+		ASpell& operator=(const ASpell& origine);
+		ASpell(const std::string& name, const std::string& effects);
+		virtual ~ASpell();
+
+		const std::string& getName() const;
+		const std::string& getEffects() const;
+
+		virtual ASpell* clone() = 0;
+
+
+};
