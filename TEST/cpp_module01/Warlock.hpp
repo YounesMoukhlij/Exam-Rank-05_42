c64155da89bc5c450d8f5353a6231d51e077aff0
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-11-01 15:15:54 +0100

    by gacp

--- TEST/cpp_module01/Warlock.hpp
+++ TEST/cpp_module01/Warlock.hpp
@@ -21,9 +21,11 @@ class Warlock
 		void	setTitle(const std::string& title);
 		void	learnSpell(const ASpell* spell);
 		void	forgetSpell(std::string spell);
-		void	launchSpell(std::string spell, ATarget& )
+		void	launchSpell(std::string spell, ATarget& target);
 
 };
+
+
 
 
 
