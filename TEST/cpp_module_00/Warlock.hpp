4012d20285849e7e32970e22220234d939788191
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-11-01 13:59:04 +0100

    by gacp

--- TEST/cpp_module_00/Warlock.hpp
+++ TEST/cpp_module_00/Warlock.hpp
@@ -10,10 +10,10 @@ class Warlock
 		std::string _name;
 		std::string _title;
 	public:
+		Warlock& operator=(const Warlock& origine);
 		Warlock(const std::string& name, const std::string& title);
 		~Warlock();
 
-		Warlock& operator=(const Warlock& origine);
 };
 
 Warlock::Warlock(/* args */)
