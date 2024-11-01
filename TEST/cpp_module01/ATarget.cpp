d0c10e790b40fdb8a0c17921bd58c1aadd0e10e5
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-11-01 15:42:24 +0100

    by gacp

---
+++ TEST/cpp_module01/ATarget.cpp
@@ -0,0 +1,34 @@
+
+
+void ATarget::getHitBySpell(const ASpell aspell)
+{
+	std::cout << _type << " has been " << aspell.getEffects() << "!" << std::endl;
+}
+
+
+ATarget::ATarget(const std::string& type) : _type(type)
+{
+
+}
+
+ATarget::~ATarget()
+{
+
+}
+
+
+ATarget&  ATarget::operator=(const ATarget& origine)
+{
+	if (this != &origine)
+	{
+		_type = origine._type;
+	}
+	return (*this);
+}
+
+
+
+const std::string& ATarget::getType() const
+{
+	return _type;
+}
--- TEST/cpp_module01/ATarget.hpp
+++ TEST/cpp_module01/ATarget.hpp
@@ -20,35 +20,3 @@ class ATarget
 
 };
 
-void ATarget::getHitBySpell(const ASpell aspell)
-{
-	std::cout << _type << " has been " << aspell.getEffects() << "!" << std::endl;
-}
-
-
-ATarget::ATarget(const std::string& type) : _type(type)
-{
-
-}
-
-ATarget::~ATarget()
-{
-
-}
-
-
-ATarget&  ATarget::operator=(const ATarget& origine)
-{
-	if (this != &origine)
-	{
-		_type = origine._type;
-	}
-	return (*this);
-}
-
-
-
-const std::string& ATarget::getType() const
-{
-	return _type;
-}
