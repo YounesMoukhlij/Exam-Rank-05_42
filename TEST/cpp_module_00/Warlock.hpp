b1a33c76f0029516d1b45d999fc44b21d99c441f
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-11-01 13:58:28 +0100

    by gacp

--- TEST/cpp_module_00/Warlock.hpp
+++ TEST/cpp_module_00/Warlock.hpp
@@ -10,7 +10,7 @@ class Warlock
 		std::string _name;
 		std::string _title;
 	public:
-		Warlock(const std::string& name, co;
+		Warlock(const std::string& name, const std::string& title);
 		~Warlock();
 };
 
