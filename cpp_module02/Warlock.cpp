d3fd8597cb43f4ee4090494a6f116bc117c5e63c
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-11-03 19:09:19 +0100

    by gacp

--- cpp_module02/Warlock.cpp
+++ cpp_module02/Warlock.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/03 17:40:12 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/11/03 19:09:03 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/11/03 19:09:18 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,18 +55,14 @@ Warlock& Warlock::operator=(const Warlock& origine)
 
 void	Warlock::learnSpell(ASpell* spell)
 {
-	M
-	if (spell)
-	{
-		if (MAP.find(spell->getName()) == MAP.end())
-			MAP[spell->getName()] = spell->clone();
-	}
+	MAP.learnspell(spell);
 }
 
 
 void	Warlock::forgetSpell(std::string spell)
 
 {
+	
 	if (MAP.find(spell) != MAP.end())
 	{
 		delete MAP[spell];
