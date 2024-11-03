f97706b7090899bc7dc7a4b9d09c26866c3f12ae
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-11-03 18:58:28 +0100

    by gacp

--- cpp_module02/TargetGenerator.hpp
+++ cpp_module02/TargetGenerator.hpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/03 18:54:51 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/11/03 18:58:07 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/11/03 18:58:27 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ class TargetGenerator
 	private :
 		TargetGenerator(const TargetGenerator& origine);
 		TargetGenerator& operator=(const TargetGenerator& origine);
-		std::map<std::string, ASpell* > MAP;
+		std::map<std::string, ATarget* > Tar;
 	public:
 		void learnTargetType(ATarget*);
 		void forgetTargetType(string const &);
