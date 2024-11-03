4bba4084fb2eb411eb95468bca7816394ec1c5ba
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-11-03 19:11:47 +0100

    by gacp

--- cpp_module02/Warlock.cpp
+++ cpp_module02/Warlock.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/03 17:40:12 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/11/03 19:11:31 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/11/03 19:11:39 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -69,5 +69,5 @@ void	Warlock::forgetSpell(std::string spell)
 void	Warlock::launchSpell(std::string spell, const ATarget& target)
 {
 	if (MAP.createSpell(spell))
-	MAP.createSpell(spell)->launch(target);
+		MAP.createSpell(spell)->launch(target);
 }
