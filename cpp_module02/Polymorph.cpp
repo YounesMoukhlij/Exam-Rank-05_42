03ed720d999f68ad5139b6f131292e1f7677542f
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-11-03 18:35:20 +0100

    by gacp

--- cpp_module02/Polymorph.cpp
+++ cpp_module02/Polymorph.cpp
@@ -1,3 +1,17 @@
 
 
-#include ""
+#include "Polymorph.hpp"
+
+
+Polymorph::Polymorph(/* args */) : ASpell("Polymorph", "turned into a critter")
+{
+}
+
+Polymorph::~Polymorph()
+{
+}
+
+ASpell* Polymorph::clone() const
+{
+	return new Polymorph();
+}
