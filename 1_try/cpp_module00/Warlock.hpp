abed72e8547186983bbc62598e365e99145ed0bb
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-11-03 16:22:21 +0100

    by gacp

--- 1_try/cpp_module00/Warlock.hpp
+++ 1_try/cpp_module00/Warlock.hpp
@@ -57,5 +57,5 @@ void	Warlock::setTitle(const std::string& title)
 
 void introduce() const
 {
-	std::cout << _name << ": I am " << _name << ", " << _title << "!"
+	std::cout << _name << ": I am " << _name << ", " << _title << "!" << std::endl;
 }
