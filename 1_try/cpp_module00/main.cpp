68c87a36ffec44d0d41de057a3726427a8978833
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-11-03 16:29:47 +0100

    by gacp

--- 1_try/cpp_module00/main.cpp
+++ 1_try/cpp_module00/main.cpp
@@ -4,8 +4,8 @@
 int main()
 {
   Warlock const richard("Richard", "Mistress of Magma");
-  richard.introduce();
-  std::cout << richard.getName() << " - " << richard.getTitle() << std::endl;
+//   richard.introduce();
+//   std::cout << richard.getName() << " - " << richard.getTitle() << std::endl;
 
 //   Warlock* jack = new Warlock("Jack", "the Long");
 //   jack->introduce();
