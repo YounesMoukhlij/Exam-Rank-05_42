7bf1b508981f86fb5d59604f466eb21b88a62061
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-11-03 18:36:24 +0100

    by gacp

--- cpp_module02/Fireball.cpp
+++ cpp_module02/Fireball.cpp
@@ -1,2 +1,16 @@
 
-#incl
+#include "Fireball.hpp"
+
+
+Fireball::Fireball(/* args */) : ASpell("Fireball", "burnt to a crisp")
+{
+}
+
+Fireball::~Fireball()
+{
+}
+
+ASpell* Fireball::clone() const
+{
+	return new Fireball();
+}
