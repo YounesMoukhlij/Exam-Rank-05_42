d296d36ce1ad23c9be242c30186ee953b9bfa1ed
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-11-03 18:59:47 +0100

    by gacp

--- cpp_module02/TargetGenerator.hpp
+++ cpp_module02/TargetGenerator.hpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/03 18:54:51 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/11/03 18:59:26 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/11/03 18:59:46 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,51 +36,3 @@ class TargetGenerator
 
 
 
-void TargetGenerator::learnSpell(ATarget* spell)
-{
-	if (spell)
-	{
-		MAP[spell->getName()] = spell->clone();
-	}
-}
-TargetGenerator::~TargetGenerator()
-{
-
-}
-TargetGenerator::TargetGenerator(const TargetGenerator& origine)
-{
-	this = &origine;
-}
-
-TargetGenerator::TargetGenerator()
-{
-
-}
-
-
-
-void TargetGenerator::forgetSpell(std::string const & str )
-{
-	std::map<std::string, ATarget * >::iterator it = MAP.find(str);
-	if (it != Tar.end())
-	{
-		delete it->second;
-		Tar.erase(it);
-	}
-}
-
-		ASpell* TargetGenerator::createSpell(std::string const & str)
-		{
-			ASpell * t = 0;
-
-			if (Tar.find(str) != Tar.end())
-				t = Tar[str];
-			return (t);
-		}
-
-
-TargetGenerator& TargetGenerator::operator=(const TargetGenerator& origine)
-{
-	Tar = origine._Tar;
-	return *this;
-}
