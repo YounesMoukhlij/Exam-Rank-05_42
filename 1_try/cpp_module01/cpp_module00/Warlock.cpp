d47a73ebf728ffa2c60df717dc39c75111bfa9a9
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-11-03 16:42:06 +0100

    by gacp

---
+++ 1_try/cpp_module01/cpp_module00/Warlock.cpp
@@ -0,0 +1,42 @@
+
+
+#include "Warlock.hpp"
+
+Warlock::Warlock(const std::string& name, const std::string& title) : _name(name), _title(title)
+{
+	std::cout << _name << ": This looks like another boring day." << std::endl;
+}
+
+Warlock::~Warlock()
+{
+	std::cout << _name << ": My job here is done!" << std::endl;
+}
+
+const std::string&   Warlock::getName() const
+{
+	return _name;
+}
+
+const std::string&  Warlock::getTitle() const
+{
+	return _title;
+}
+
+void	Warlock::setTitle(const std::string& title)
+{
+	_title = title;
+}
+
+
+
+void Warlock::introduce() const
+{
+	std::cout << _name << ": I am " << _name << ", " << _title << "!" << std::endl;
+}
+
+Warlock& Warlock::operator=(const Warlock& origine)
+{
+		_name = origine._name;
+		_title = origine._title;
+	return *this;
+}
