10f6773c3ffe1fb57acaa3529f2586838efd3641
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-11-03 18:43:07 +0100

    by gacp

--- cpp_module02/SpellBook.hpp
+++ cpp_module02/SpellBook.hpp
@@ -11,10 +11,13 @@ class SpellBook
 {
 	private :
 		SpellBook(const SpellBook& origine);
-		~SpellBook(const SpellBook& origine);
+		operat(const SpellBook& origine);
 		std::map<std::string, ASpell* > MAP;
 	public:
 		void learnSpell(ASpell*);
 		void forgetSpell(string const &);
 		ASpell* createSpell(string const &);
+
+
+		~SpellBook();
 }
