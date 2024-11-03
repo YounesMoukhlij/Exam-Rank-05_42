028fab1d0ff01bdce530ff5c0a256025b9ba49aa
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-11-03 19:00:09 +0100

    by gacp

--- cpp_module02/TargetGenerator.cpp
+++ cpp_module02/TargetGenerator.cpp
@@ -1,3 +1,54 @@
 
 
-#include "Target"
+#include "TargetGenerator.hpp"
+
+
+
+void TargetGenerator::learnSpell(ATarget* spell)
+{
+	if (spell)
+	{
+		MAP[spell->getName()] = spell->clone();
+	}
+}
+TargetGenerator::~TargetGenerator()
+{
+
+}
+TargetGenerator::TargetGenerator(const TargetGenerator& origine)
+{
+	this = &origine;
+}
+
+TargetGenerator::TargetGenerator()
+{
+
+}
+
+
+
+void TargetGenerator::forgetSpell(std::string const & str )
+{
+	std::map<std::string, ATarget * >::iterator it = MAP.find(str);
+	if (it != Tar.end())
+	{
+		delete it->second;
+		Tar.erase(it);
+	}
+}
+
+		ASpell* TargetGenerator::createSpell(std::string const & str)
+		{
+			ASpell * t = 0;
+
+			if (Tar.find(str) != Tar.end())
+				t = Tar[str];
+			return (t);
+		}
+
+
+TargetGenerator& TargetGenerator::operator=(const TargetGenerator& origine)
+{
+	Tar = origine._Tar;
+	return *this;
+}
