85729e43e47575b5a3429a9c237af3c288f9ad50
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-11-03 17:36:32 +0100

    by gacp

--- 1_try/cpp_module01/Warlock.cpp
+++ 1_try/cpp_module01/Warlock.cpp
@@ -40,3 +40,32 @@ Warlock& Warlock::operator=(const Warlock& origine)
 		_title = origine._title;
 	return *this;
 }
+
+
+void	Warlock::learnSpell(ASpell* spell)
+{
+	if (spell)
+	{
+		if (MAP.find(spell->getName()) == MAP.end())
+			MAP[spell->getName()] = spell.clone();
+	}
+}
+
+
+void	Warlock::forgetSpell(std::string spell)
+
+{
+	if (MAP.find(spell) != MAP.end())
+	{
+		delete MAP[spell];
+		MAP.erase(MAP.find(spell));
+	}
+}
+
+void	Warlock::launchSpell(std::string spell, const ATarget& target)
+{
+	if (MAP.find(spell) != MAP.end())
+	{
+		MAP[spell]->launch(target);
+	}
+}
