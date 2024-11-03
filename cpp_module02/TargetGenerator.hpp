07c5934a990aa0487c7d2548ca146c88825d4415
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-11-03 18:57:34 +0100

    by gacp

--- cpp_module02/TargetGenerator.hpp
+++ cpp_module02/TargetGenerator.hpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/03 18:54:51 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/11/03 18:57:19 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/11/03 18:57:32 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,21 @@
 
 class TargetGenerator
 {
-		
+	publi	
+};
+
+class SpellBook
+{
+	private :
+		SpellBook(const SpellBook& origine);
+		SpellBook& operator=(const SpellBook& origine);
+		std::map<std::string, ASpell* > MAP;
+	public:
+		void learnSpell(ASpell* spell);
+		void forgetSpell(std::string const & str);
+		ASpell* createSpell(std::string const & str);
+
+
+		~SpellBook();
+		SpellBook();
 };
