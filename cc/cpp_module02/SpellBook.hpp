8ae72a2b50164c180f475372aa399c10e0082042
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-11-05 13:10:57 +0100

    by gacp

--- cc/cpp_module02/SpellBook.hpp
+++ cc/cpp_module02/SpellBook.hpp
@@ -55,3 +55,27 @@ class SpellBook
 			if (MAP.find(spell) != MAP.end())
 				MAP[spell]->launch(target);
 		}
+
+		Warlock::Warlock(const Warlock& origine)
+{
+	*this = origine;
+}
+
+
+		Warlock::Warlock(const std::string& name, const std::string& title) : _name(name), _title(title)
+		{
+			std::cout << _name << ": This looks like another boring day." << std::endl;
+		}
+
+		Warlock& Warlock::operator=(const Warlock& origine)
+		{
+			_name = origine._name;
+			_title = origine._title;
+			return *this;
+		}
+
+
+		Warlock::~Warlock()
+		{
+			std::cout << _name << ": My job here is done!" << std::endl;
+		}
