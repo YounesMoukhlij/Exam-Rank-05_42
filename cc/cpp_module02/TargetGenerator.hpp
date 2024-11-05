897bcecd67ee1b195c42e79315e5b85205509721
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-11-05 13:23:01 +0100

    by gacp

--- cc/cpp_module02/TargetGenerator.hpp
+++ cc/cpp_module02/TargetGenerator.hpp
@@ -28,3 +28,52 @@ class TargetGenerator
 
 
 };
+
+
+		void SpellBook::learnSpell(ASpell* spell)
+		{
+			if (spell)
+			{
+				if (MAP.find(spell->getName()) == MAP.end())
+				{
+					MAP[spell->getName()] = spell->clone();
+				}
+			}
+		}
+
+		void SpellBook::forgetSpell(std::string& spell)
+		{
+			if (MAP.find(spell) != MAP.end())
+			{
+				MAP.erase(MAP.find(spell));
+			}
+		}
+		ASpell* SpellBook::createSpell(std::string& spell)
+		{
+			ASpell *u;
+
+			if (MAP.find(spell) != MAP.end())
+			{
+				u = MAP[spell];
+			}
+			return u;
+		}
+
+		SpellBook::SpellBook(const SpellBook& origine)
+{
+	*this = origine;
+}
+
+
+
+		SpellBook& SpellBook::operator=(const SpellBook& origine)
+		{
+			MAP = origine.MAP;
+			return *this;
+		}
+
+
+		SpellBook::~SpellBook()
+		{
+
+		}
