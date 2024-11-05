a8c0aa169154034282b2900fb7bfb099474e8337
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-11-05 12:43:27 +0100

    by gacp

--- cc/cpp_module01/Dummy.hpp
+++ cc/cpp_module01/Dummy.hpp
@@ -11,17 +11,3 @@ class Dummy : public ATarget
 };
 
 
-ATarget* Dummy::clone () const
-{
-	return new Dummy();
-}
-
-Dummy::Dummy() : ATarget("Target Practice Dummy")
-{
-
-}
-
-Dummy::~Dummy()
-{
-
-}
