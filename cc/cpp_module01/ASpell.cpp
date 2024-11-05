6671ec9bf29f40cff6bf2a49ec0b2c4a2b06201e
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-11-05 12:15:36 +0100

    by gacp

--- cc/cpp_module01/ASpell.cpp
+++ cc/cpp_module01/ASpell.cpp
@@ -1,32 +1,4 @@
 
-
-
-#pragma once 
-
-#include <iostream>
-
-class ASpell
-{
-	private:
-		std::string _name;
-		std::string _effects;
-	public:
-		ASpell(const std::string& name, const std::string& effects);
-		ASpell(const ASpell& origine);
-
-		ASpell& operator=(const ASpell& origine);
-
-		const std::string& getName() const;
-		const std::string& getEffects() const;
-
-		virtual ~ASpell();
-		virtual	ASpell* clone () const = 0;
-};
-
-
-
-
-
 #include "ASpell.hpp"
 
 ASpell::ASpell(const ASpell& origine)
@@ -63,5 +35,4 @@ ASpell::ASpell(const ASpell& origine)
 		{
 			return _effects;
 		}
-
 
