d2ca77089a46daaf83d73deb754b030f5c62ae8a
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-11-05 12:54:46 +0100

    by gacp

---
+++ cc/cpp_module02/ASpell.cpp
@@ -0,0 +1,42 @@
+
+#include "ASpell.hpp"
+
+ASpell::ASpell(const ASpell& origine)
+{
+	*this = origine;
+}
+
+
+		ASpell::ASpell(const std::string& name, const std::string& effects) : _name(name), _effects(effects)
+		{
+
+		}
+
+		ASpell& ASpell::operator=(const ASpell& origine)
+		{
+			_name = origine._name;
+			_effects = origine._effects;
+			return *this;
+		}
+
+
+		ASpell::~ASpell()
+		{
+
+		}
+
+
+
+		const std::string& ASpell::getName() const
+		{
+			return _name;
+		}
+		const std::string& ASpell::getEffects() const
+		{
+			return _effects;
+		}
+
+	void  ASpell::launch(const ATarget& target) const
+	{
+		target.getHitBySpell(this);
+	}
---
+++ cc/cpp_module02/ATarget.hpp
@@ -0,0 +1,27 @@
+
+
+#pragma once
+
+#include "ASpell.hpp"
+
+class ASpell;
+
+class ATarget
+{
+	protected:
+		std::string _type;
+	public:
+		void getHitBySpell(const ASpell* spell) const ;
+
+		ATarget(const std::string& type);
+		ATarget(const ATarget& origine);
+
+		ATarget& operator=(const ATarget& origine);
+
+		const std::string& getType() const;
+
+		virtual ~ATarget();
+		virtual	ATarget* clone () const = 0;
+};
+
+
