a03027896eccb14ffaea2ec2e3a613eb9d22b59f
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-11-05 12:17:36 +0100

    by gacp

--- cc/cpp_module01/ATarget.hpp
+++ cc/cpp_module01/ATarget.hpp
@@ -12,4 +12,14 @@ class ATarget
 		std::string _type;
 	public:
 		void getHitBySpell(const ASpell* spell);
+		ASpell(const std::string& type);
+		ASpell(const ASpell& origine);
+
+		ASpell& operator=(const ASpell& origine);
+
+		const std::string& getName() const;
+		const std::string& getEffects() const;
+
+		virtual ~ASpell();
+		virtual	ASpell* clone () const = 0;
 };
