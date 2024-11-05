9b5d235143271e3f55ff4db00ce1b3140c0718b4
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-11-05 12:22:35 +0100

    by gacp

--- cc/cpp_module01/Fwoosh.hpp
+++ cc/cpp_module01/Fwoosh.hpp
@@ -8,13 +8,7 @@ class Fwoosh
 		std::string _effects;
 	public:
 		Fwoosh(const std::string& name, const std::string& effects);
-		Fwoosh(const Fwoosh& origine);
 
-		Fwoosh& operator=(const Fwoosh& origine);
-
-		const std::string& getName() const;
-		const std::string& getEffects() const;
-
-		 ~Fwoosh();
+		 	~Fwoosh();
 			Fwoosh* clone () const = 0;
 };
