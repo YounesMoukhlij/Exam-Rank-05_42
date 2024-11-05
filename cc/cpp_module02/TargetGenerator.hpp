2c4e40fc3859c5c34909ae0fe8844fe900ecac2e
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-11-05 13:29:21 +0100

    by gacp

--- cc/cpp_module02/TargetGenerator.hpp
+++ cc/cpp_module02/TargetGenerator.hpp
@@ -23,7 +23,7 @@ class TargetGenerator
 
 		void learnTargetType(ATarget* target);
 		void forgetTargetType(std::string const & target);
-		ATarget* createTarget(std::string const & target)
+		ATarget* createTarget(std::string const & target);
 
 
 };
