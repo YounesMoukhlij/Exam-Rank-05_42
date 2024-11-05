fa89dfd995268205951ec5be7dae655759a5519e
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-11-05 12:20:22 +0100

    by gacp

--- cc/cpp_module01/ATarget.hpp
+++ cc/cpp_module01/ATarget.hpp
@@ -59,5 +59,5 @@ ATarget::ATarget(const ATarget& origine)
 
 		void ATarget::getHitBySpell(const ASpell* spell)
 		{
-			std::cout << _type << " has been "<EFFECTS>"!" << std::endl;
+			std::cout << _type << " has been "<< spell.getEffects() << "!" << std::endl;
 		}
