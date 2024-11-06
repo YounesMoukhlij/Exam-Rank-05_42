505e33e3b8e60deb6dcb8eb138dbef0e9faeea24
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-11-06 16:38:04 +0100

    by gacp

---
+++ 3a/Fireball.hpp
@@ -0,0 +1,19 @@
+
+#pragma once
+
+
+#include <iostream>
+#include "ATarget.hpp"
+#include "ASpell.hpp"
+
+
+class Fwoosh : public ASpell
+{
+	public:
+		Fwoosh();
+		~Fwoosh();
+		ASpell* clone () const;
+
+
+
+};
