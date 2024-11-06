872e22d5e7786ae38bd3670694fa9152c1dbe3e6
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-11-06 15:58:55 +0100

    by gacp

--- 3a/ATarget.hpp
+++ 3a/ATarget.hpp
@@ -12,11 +12,11 @@ class ASpell;
 class ATarget
 {
 	protected:
+		std::string	_type;
+	public:
 		ATarget();
 		ATarget(const ATarget& origine);
 		ATarget& operator=(const ATarget& origine);
-		std::string	_type;
-	public:
 		ATarget(const std::string& type);
 		virtual ~ATarget();
 
