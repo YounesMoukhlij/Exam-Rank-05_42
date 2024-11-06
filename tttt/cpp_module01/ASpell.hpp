5cd4350dcf9a00f7447be2944685c2a2f26af24c
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-11-06 15:03:53 +0100

    by gacp

--- tttt/cpp_module01/ASpell.hpp
+++ tttt/cpp_module01/ASpell.hpp
@@ -3,3 +3,24 @@
 
 
 #include <iostream>
+
+
+class A
+{
+	private:
+		A();
+		A(const A& origine);
+		A& operator=(const A& origine);
+		std::string	name;
+		std::string	title;
+	public:
+		A(const std::string& _name, const std::string&  _title);
+		~A();
+
+
+		const std::string&    	getName() const;
+		const std::string&	 	getTitle() const;
+		void					introduce() const;
+		void					setTitle(const std::string& _title);
+
+};
