7c26a2e8a4dca22ebe727cb04b2f58d07e1bc6fa
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-11-06 16:58:33 +0100

    by gacp

--- 3a/SpellBook.cpp
+++ 3a/SpellBook.cpp
@@ -1,1 +1,50 @@
-#include "ASpell.hpp"
+#include "SpellBook.hpp"
+
+
+		SpellBook::SpellBook(const SpellBook& origine)
+		{
+			*this = origine;
+		}
+
+
+SpellBook& SpellBook::operator=(const SpellBook& origine)
+{
+		MAP = origine.MAP;
+	return *this;
+}
+		SpellBook::~SpellBook()
+		{}
+		SpellBook::SpellBook()
+		{}
+
+		void	SpellBook::learnSpell(ASpell* spell)
+		{
+			if (spell)
+			{
+				if (MAP[spell->getName()] == MAP.end())
+				{
+					MAP[spell->getName()] = spell->clone();
+				}
+			}
+		}
+		void	SpellBook::forgetSpell(const std::string& spell)
+		{
+
+			if (MAP[spell] != MAP.end())
+			{
+				MAP.erase(MAP[spell]);
+			}
+
+
+		}
+		ASpell*	SpellBook::createSpell(std::string& spell)
+		{
+			ASpell* t = 0;
+
+
+			if (MAP[spell] != MAP.end())
+			{
+				t = MAP[spell];
+			}
+			return t;
+		}
