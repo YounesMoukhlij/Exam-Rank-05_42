68efa8244768d7165e1e7776c349d11206361011
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-11-06 15:06:27 +0100

    by gacp

--- tttt/cpp_module01/ASpell.hpp
+++ tttt/cpp_module01/ASpell.hpp
@@ -31,3 +31,41 @@ class ASpell
 };
 
 
+ASpell::ASpell(const std::string& name, const std::string& title) : _name(name), _title(title)
+{
+	std::cout << _name << ": This looks like another boring day." << std::endl;
+}
+
+ASpell::~ASpell()
+{
+	std::cout << _name << ": My job here is done!" << std::endl;
+}
+
+const std::string&   ASpell::getName() const
+{
+	return _name;
+}
+
+const std::string&  ASpell::getTitle() const
+{
+	return _title;
+}
+
+void	ASpell::setTitle(const std::string& title)
+{
+	_title = title;
+}
+
+
+
+void ASpell::introduce() const
+{
+	std::cout << _name << ": I am " << _name << ", " << _title << "!" << std::endl;
+}
+
+ASpell& ASpell::operator=(const ASpell& origine)
+{
+		_name = origine._name;
+		_title = origine._title;
+	return *this;
+}
