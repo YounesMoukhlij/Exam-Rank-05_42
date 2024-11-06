99a72d8c39c73bbde1c6bd56e4ed04d891a49c29
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-11-06 17:10:10 +0100

    by gacp

--- 3a/TargetGenerator.hpp
+++ 3a/TargetGenerator.hpp
@@ -23,40 +23,8 @@ class TargetGenerator
 
 		void	learnTargetType(ATarget* spell);
 		void	forgetTargetType(const std::string& spell);
-		ATarget*	createTarget(std::string& spell);
+		ATarget*	createTarget(const std::string& spell);
 
 };
 
 
-
-		void	TargetGenerator::learnTargetType(ATarget* spell)
-		{
-			if (spell)
-			{
-				if (MAP.find(spell->getType()) == MAP.end())
-				{
-					MAP[spell->getType()] = spell->clone();
-				}
-			}
-		}
-		void	TargetGenerator::forgetTargetType(const std::string& spell)
-		{
-
-			if (MAP.find(spell) != MAP.end())
-			{
-				MAP.erase(MAP.find(spell));
-			}
-
-
-		}
-		ATarget*	TargetGenerator::createTarget(const std::string& spell)
-		{
-			ATarget* t = 0;
-
-
-			if (MAP.find(spell) != MAP.end())
-			{
-				t = MAP[spell];
-			}
-			return t;
-		}
