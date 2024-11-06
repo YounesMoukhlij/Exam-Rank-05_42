4015b4f2299e949c0e050cd5848dd14af0b2aeaf
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-11-06 16:54:31 +0100

    by gacp

--- 3a/SpellBook.hpp
+++ 3a/SpellBook.hpp
@@ -46,7 +46,7 @@ SpellBook& SpellBook::operator=(const SpellBook& origine)
 
 		void	SpellBook::learnSpell(ASpell* spell)
 		{
-			std::map<std::string, ASpell *>::iterator it = MAP.find(spell.getName());
+
 			if (it != MAP.end())
 			{
 				
