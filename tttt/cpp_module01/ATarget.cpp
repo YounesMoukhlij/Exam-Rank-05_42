213844a31ec784920381c0ab321b3bcd1c7f1778
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-11-06 15:47:09 +0100

    by gacp

--- tttt/cpp_module01/ATarget.cpp
+++ tttt/cpp_module01/ATarget.cpp
@@ -37,3 +37,37 @@ ATarget& ATarget::operator=(const ATarget& origine)
 	_type = origine._type;
 	return ;
 }
+
+
+Warlock::Warlock(const std::string& _name, const std::string& _title) : name(_name), title(_title)
+{
+	std::cout << _name << " : This looks like another boring day." << std::endl;
+}
+
+Warlock::~Warlock()
+{
+	std::cout << name << ": My job here is done!" << std::endl;
+}
+
+const std::string& 	Warlock::getName() const
+{
+	return (name);
+}
+
+const std::string& 	Warlock::getTitle() const
+{
+	return (title);	
+}
+
+void	Warlock::introduce() const
+{
+	std::cout << name << ": I am " << name << ", " << title << "!" << std::endl;
+	
+}
+
+Warlock& Warlock::operator=(const Warlock& origine)
+{
+	this->name = origine.name;
+	this->title = origine.title;
+	return (*this);
+}
