ecbf96c3686f402adcf03390a7a054d4c53b0bdb
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-11-06 15:30:32 +0100

    by gacp

--- tttt/cpp_module01/Warlock.hpp
+++ tttt/cpp_module01/Warlock.hpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 14:20:11 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/11/06 15:30:12 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/11/06 15:30:32 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,7 +44,10 @@ class Warlock
 
 		void	Warlock::learnSpell(ASpell* spell)
 		{
-			if (MAP.find(spell) ==)
+			if (MAP.find(spell) == MAP.end())
+			{
+				Ma
+			}
 		}
 		void	Warlock::forgetSpell(std::string spell);
 		void	Warlock::launchSpell(std::string spell,  ATarget& tar);
