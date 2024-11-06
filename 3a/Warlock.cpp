734cb393b592796f3e45b4f4f02971bae85b4415
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-11-06 16:00:16 +0100

    by gacp

--- 3a/Warlock.cpp
+++ 3a/Warlock.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 14:20:09 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/11/06 15:53:49 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/11/06 16:00:09 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,7 +66,7 @@ void	Warlock::setTitle(const std::string& _title)
 				MAP.erase(MAP.find(spell));
 			}
 		}
-		void	Warlock::launchSpell(std::string spell,  ATarget& tar)
+		void	Warlock::launchSpell(std::string spell, const ATarget& tar)
 		{
 			if (MAP.find(spell) != MAP.end())
 			{
