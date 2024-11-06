5ee1a9a47f663ecf54e966cca3f3b66331b11117
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-11-06 15:25:34 +0100

    by gacp

---
+++ tttt/cpp_module01/Warlock.cpp
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Warlock.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/17 14:20:09 by youmoukh          #+#    #+#             */
+/*   Updated: 2024/10/17 14:49:12 by youmoukh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Warlock.hpp"
+
+Warlock::Warlock(const std::string& _name, const std::string& _title) : name(_name), title(_title)
+{
+	std::cout << _name << " : This looks like another boring day." << std::endl;
+}
+
+Warlock::~Warlock()
+{
+	std::cout << name << ": My job here is done!" << std::endl;
+}
+
+const std::string& 	Warlock::getName() const
+{
+	return (name);
+}
+
+const std::string& 	Warlock::getTitle() const
+{
+	return (title);	
+}
+
+void	Warlock::introduce() const
+{
+	std::cout << name << ": I am " << name << ", " << title << "!" << std::endl;
+	
+}
+
+Warlock& Warlock::operator=(const Warlock& origine)
+{
+	this->name = origine.name;
+	this->title = origine.title;
+	return (*this);
+}
+
+void	Warlock::setTitle(const std::string& _title)
+{
+	title = _title;
+}
