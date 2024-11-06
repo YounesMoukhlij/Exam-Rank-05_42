3654202738b719fee9a2afef8521e1c15ff9f16f
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-11-06 16:27:09 +0100

    by gacp

--- 3a/Warlock.cpp
+++ 3a/Warlock.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 14:20:09 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/11/06 16:26:50 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/11/06 16:26:54 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,12 @@
 
 Warlock::Warlock(const std::string& _name, const std::string& _title) : name(_name), title(_title)
 {
-	std::cout << _name << " : This looks like another boring day." << std::endl;
+	std::cout << _name << ": This looks like another boring day." << std::endl;
 }
 
 Warlock::~Warlock()
 {
-	std::cout << name << " : My job here is done!" << std::endl;
+	std::cout << name << ": My job here is done!" << std::endl;
 }
 
 const std::string& 	Warlock::getName() const
