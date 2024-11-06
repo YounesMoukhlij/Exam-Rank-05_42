0393b9415d74b3f2c6762c31686f5bdd9e514398
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-11-06 15:47:22 +0100

    by gacp

--- tttt/cpp_module01/ATarget.cpp
+++ tttt/cpp_module01/ATarget.cpp
@@ -39,33 +39,33 @@ ATarget& ATarget::operator=(const ATarget& origine)
 }
 
 
-Warlock::Warlock(const std::string& _name, const std::string& _title) : name(_name), title(_title)
+ATarget::ATarget(const std::string& _name, const std::string& _title) : name(_name), title(_title)
 {
 	std::cout << _name << " : This looks like another boring day." << std::endl;
 }
 
-Warlock::~Warlock()
+ATarget::~ATarget()
 {
 	std::cout << name << ": My job here is done!" << std::endl;
 }
 
-const std::string& 	Warlock::getName() const
+const std::string& 	ATarget::getName() const
 {
 	return (name);
 }
 
-const std::string& 	Warlock::getTitle() const
+const std::string& 	ATarget::getTitle() const
 {
 	return (title);	
 }
 
-void	Warlock::introduce() const
+void	ATarget::introduce() const
 {
 	std::cout << name << ": I am " << name << ", " << title << "!" << std::endl;
 	
 }
 
-Warlock& Warlock::operator=(const Warlock& origine)
+ATarget& ATarget::operator=(const ATarget& origine)
 {
 	this->name = origine.name;
 	this->title = origine.title;
