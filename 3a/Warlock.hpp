b2690b7a2305e132cb836289bb4c1783e3bdd899
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-11-06 16:41:33 +0100

    by gacp

--- 3a/Warlock.hpp
+++ 3a/Warlock.hpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 14:20:11 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/11/06 16:09:35 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/11/06 16:41:32 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 #include <iostream>
 #include <map>
 #include "ASpell.hpp"
+#include "SpellBook.hpp"
 
 
 class Warlock
