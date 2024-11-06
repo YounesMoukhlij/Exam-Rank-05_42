17b56bd02dc749e769f61066d414de7006b665c4
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-11-06 15:35:10 +0100

    by gacp

--- tttt/cpp_module01/Warlock.hpp
+++ tttt/cpp_module01/Warlock.hpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 14:20:11 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/11/06 15:34:58 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/11/06 15:35:03 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,7 +46,7 @@ class Warlock
 		{
 			if (MAP.find(spell->getName()) == MAP.end())
 			{
-				MAP[spell.getName()] = spell->clone();
+				MAP[spell->getName()] = spell->clone();
 			}
 		}
 		void	Warlock::forgetSpell(std::string spell)
