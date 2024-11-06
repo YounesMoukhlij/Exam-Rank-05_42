2bdd5a7b7489cc0d30ffa9edc582ed91f7b89f4d
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-11-06 15:21:21 +0100

    by gacp

--- tttt/cpp_module01/Fwoosh.hpp
+++ tttt/cpp_module01/Fwoosh.hpp
@@ -1,6 +1,37 @@
 
 
+
 #pragma once
+
+
 #include <iostream>
+#include "ATarget.hpp"
+#include "A.hpp"
+
+
+class ASpell
+{
+	protected:
+		// ASpell();
+		ASpell(const ASpell& origine);
+		ASpell& operator=(const ASpell& origine);
+		std::string	_name;
+		std::string	_effects;
+	public:
+		ASpell(const std::string& name, const std::string&  effects);
+		virtual ~ASpell();
+
+
+		const std::string&    	getName() const;
+		const std::string&	 	getEffects() const;
+
+
+
+		virtual ASpell* clone () const = 0;
+
+		void launch (const ATarget& target) const;
+
+
+};
 
 
