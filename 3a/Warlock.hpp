ba195f4a5dbdba1c3fe4c7761268e010ecd9e406
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-11-06 16:41:57 +0100

    by gacp

--- 3a/Warlock.hpp
+++ 3a/Warlock.hpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 14:20:11 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/11/06 16:41:44 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/11/06 16:41:51 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ class Warlock
 		Warlock& operator=(const Warlock& origine);
 		std::string	name;
 		std::string	title;
-		
+		SpellBook MAP;
 	public:
 		Warlock(const std::string& _name, const std::string&  _title);
 		~Warlock();
