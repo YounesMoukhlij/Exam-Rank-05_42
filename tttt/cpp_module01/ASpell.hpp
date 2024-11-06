b26bc7be5df546e4e659f5566069e6f529e978e0
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-11-06 15:49:14 +0100

    by gacp

--- tttt/cpp_module01/ASpell.hpp
+++ tttt/cpp_module01/ASpell.hpp
@@ -31,37 +31,3 @@ class ASpell
 
 
 };
-		void ASpell::launch (const ATarget& target) const
-		{
-			target.getHitBySpell(this);
-		}
-
-
-ASpell::ASpell(const std::string& name, const std::string& title) : _name(name), _effects(effects)
-{
-
-}
-
-ASpell::~ASpell()
-{
-
-}
-
-const std::string&   ASpell::getName() const
-{
-	return _name;
-}
-
-const std::string&  ASpell::getEffects() const
-{
-	return _effects;
-}
-
-
-
-ASpell& ASpell::operator=(const ASpell& origine)
-{
-		_name = origine._name;
-		_effects = origine._effects;
-	return *this;
-}
