c437c717cc0c5327997bb79f1cf9080c5d627b96
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-11-06 16:41:23 +0100

    by gacp

---
+++ 3a/Polymorph.cpp
@@ -0,0 +1,17 @@
+#include "Polymorph.hpp"
+
+ASpell* Polymorph::clone () const
+{
+	return new Polymorph();
+}
+
+
+Polymorph::Polymorph() : ASpell("Polymorph", "turned into a critter")
+{
+
+}
+
+Polymorph::~Polymorph()
+{
+
+}
--- 3a/Polymorph.hpp
+++ 3a/Polymorph.hpp
@@ -20,20 +20,3 @@ class Polymorph : public ASpell
 };
 
 
-#include "Polymorph.hpp"
-
-ASpell* Polymorph::clone () const
-{
-	return new Polymorph();
-}
-
-
-Polymorph::Polymorph() : ASpell("Polymorph", "turned into a critter")
-{
-
-}
-
-Polymorph::~Polymorph()
-{
-
-}
