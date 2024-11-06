b2ae989360075625948ebb808e8cf71ca316a0e7
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-11-06 15:50:51 +0100

    by gacp

--- tttt/cpp_module01/Warlock.cpp
+++ tttt/cpp_module01/Warlock.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 14:20:09 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/11/06 15:45:28 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/11/06 15:50:43 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -70,6 +70,6 @@ void	Warlock::setTitle(const std::string& _title)
 		{
 			if (MAP.find(spell) != MAP.end())
 			{
-				MAP[spell]->launch(target);
+				MAP[spell]->launch(tar);
 			}
 		}
