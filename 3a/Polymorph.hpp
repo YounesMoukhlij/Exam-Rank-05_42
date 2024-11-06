d11e1fffa9755ed1c2d953795410782f6ece2abe
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-11-06 16:39:09 +0100

    by gacp

---
+++ 3a/Polymorph.hpp
@@ -0,0 +1,20 @@
+
+
+#pragma once
+
+
+#include <iostream>
+#include "ATarget.hpp"
+#include "ASpell.hpp"
+
+
+class Fireball : public ASpell
+{
+	public:
+		Fireball();
+		~Fireball();
+		ASpell* clone () const;
+
+
+
+};
