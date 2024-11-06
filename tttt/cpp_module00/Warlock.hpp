6d3bb435cb291ab8656ff9b73f7e42b075944af6
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-11-06 14:40:41 +0100

    by gacp

--- tttt/cpp_module00/Warlock.hpp
+++ tttt/cpp_module00/Warlock.hpp
@@ -27,7 +27,7 @@ class Warlock
 
 		void	introduce () const
 		{
-			<NAME<< ": I am "<NAME>", "<TITLE>"!"
+			std::cout << _name << ": I am "<< _name <<", "<TITLE>"!"
 		}
 
 Warlock & Warlock::operator=(const Warlock & o)
