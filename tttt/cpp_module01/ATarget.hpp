806626db845fdeed344c96f6b7355cdb6b3f6c4f
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-11-06 15:10:26 +0100

    by gacp

--- tttt/cpp_module01/ATarget.hpp
+++ tttt/cpp_module01/ATarget.hpp
@@ -28,3 +28,33 @@ class ATarget
 
 
 };
+
+
+ASpell::ASpell(const std::string& name, const std::string& title) : _name(name), _effects(effects)
+{
+
+}
+
+ASpell::~ASpell()
+{
+
+}
+
+const std::string&   ASpell::getName() const
+{
+	return _name;
+}
+
+const std::string&  ASpell::getEffects() const
+{
+	return _effects;
+}
+
+
+
+ASpell& ASpell::operator=(const ASpell& origine)
+{
+		_name = origine._name;
+		_effects = origine._effects;
+	return *this;
+}
