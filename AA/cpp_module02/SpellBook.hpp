3e8d728951fc5f3bffbdad2f47b8019ef9bf42e0
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-11-08 15:53:54 +0100

    by gacp

--- AA/cpp_module02/SpellBook.hpp
+++ AA/cpp_module02/SpellBook.hpp
@@ -1,1 +1,35 @@
 
+
+#pragma once
+
+#include <iostream>
+#include "ASpell.hpp"
+#include "ATarget.hpp"
+#include <map>
+
+
+
+class SpellBook
+{
+	private:
+		SpellBook();
+		SpellBook(const SpellBook& origine);
+		SpellBook& operator=(const SpellBook& origine);
+		std::string	_name;
+		std::string	_title;
+		std::map<std::string, ASpell *> MAP;
+	public:
+		SpellBook(const std::string& name, const std::string&  title);
+		~SpellBook();
+
+
+		const std::string&    	getName() const;
+		const std::string&	 	getTitle() const;
+		void					introduce() const;
+		void					setTitle(const std::string& title);
+
+		void	learnSpell(ASpell * spell);
+		void	forgetSpell(std::string spell);
+		void	launchSpell(std::string spell, ATarget& tar);
+
+};
