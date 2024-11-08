4bc4afba6bb6f924dc6d88bbc1b48429202a37ea
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-11-08 15:19:27 +0100

    by gacp

--- AA/cpp_module01/Fwoosh.hpp
+++ AA/cpp_module01/Fwoosh.hpp
@@ -7,19 +7,9 @@
 
 class ASpell
 {
-	private:
-		ASpell(const ASpell& o);
-		ASpell& operator=(const ASpell& o);
-		std::string	_name;
-		std::string	_effects;
 	public:
 		ASpell();
-		virtual ~ASpell();
-
-
-		const std::string&    	getName() const;
-		const std::string&	 	getEffects() const;
-
+		 ~ASpell();
 		virtual ASpell*  clone () const = 0;
 
 
