c0af5c1b17cc1fb29136bfce0108f2c47058560f
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-11-08 15:47:40 +0100

    by gacp

---
+++ AA/cpp_module02/BrickWall.cpp
@@ -0,0 +1,16 @@
+#include "BrickWall.hpp"
+
+
+BrickWall::BrickWall() : ATarget("Target Practice BrickWall")
+{
+
+}
+		 BrickWall::~BrickWall()
+		 {
+
+		 }
+
+		 ATarget*  BrickWall::clone () const
+		 {
+			return new BrickWall();
+		 }
