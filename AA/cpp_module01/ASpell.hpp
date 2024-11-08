1a6f07e509da1eda9a69847b71445c37fa08da53
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-11-08 15:04:06 +0100

    by gacp

--- AA/cpp_module01/ASpell.hpp
+++ AA/cpp_module01/ASpell.hpp
@@ -10,7 +10,7 @@ class ASpell
 		ASpell(const ASpell& origine);
 		ASpell& operator=(const ASpell& origine);
 		std::string	_name;
-		std::string	_titl;
+		std::string	_effects;
 	public:
 		ASpell(const std::string& name, const std::string&  effects);
 		~ASpell();
@@ -21,3 +21,48 @@ class ASpell
 
 
 };
+
+#include "Warlock.hpp"
+
+void				Warlock::setTitle(const std::string& title)
+{
+	_title = title;
+}
+
+Warlock::~Warlock()
+{
+	std::cout <<  _name << ": My job here is done!" << std::endl;
+}
+
+void	Warlock::introduce() const
+{
+	std::cout << _name << ": I am " << _name << ", " << _title << "!" << std::endl;
+}
+
+Warlock::Warlock(const Warlock& o)
+{
+	*this = o;
+}
+
+Warlock::Warlock(const std::string& name, const std::string& title ) : _name (name), _title(title)
+{
+	std::cout << _name << ": This looks like another boring day." << std::endl;
+}
+
+
+
+Warlock& Warlock::operator=(const Warlock& o)
+{
+	_name = o._name;
+	_title = o._title;
+	return (*this);
+}
+
+const std::string& Warlock::getName() const
+{
+	return _name;
+}
+const std::string& Warlock::getTitle() const
+{
+	return _title;
+}
