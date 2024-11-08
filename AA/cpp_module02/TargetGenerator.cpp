1ca81513aeb17be1627d2e7c1ea0285eadabfb6a
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-11-08 16:04:03 +0100

    by gacp

---
+++ AA/cpp_module02/TargetGenerator.cpp
@@ -0,0 +1,56 @@
+
+#include "TargetGenerator.hpp"
+
+
+TargetGenerator::~TargetGenerator()
+{
+
+}
+
+
+
+TargetGenerator::TargetGenerator(const TargetGenerator& o)
+{
+	*this = o;
+}
+
+
+
+
+
+TargetGenerator& TargetGenerator::operator=(const TargetGenerator& o)
+{
+	MAP = o.MAP;
+	return (*this);
+}
+
+
+
+		void	TargetGenerator::learnTargetType(ATarget* spell)
+		{
+			if (spell)
+			{
+				if (MAP.find(spell->getName()) == MAP.end())
+					MAP[spell->getName()] = spell->clone();
+			}
+		}
+		void	TargetGenerator::forgetTargetType(std::string spell)
+		{
+				if (MAP.find(spell) != MAP.end())
+				{
+					delete MAP[spell];
+					MAP.erase(MAP.find(spell));
+				}
+
+
+		}
+		ATarget*	TargetGenerator::createTarget(std::string spell)
+		{
+			ATarget * t = 0;
+			if (MAP.find(spell) != MAP.end())
+			{
+				t = MAP[spell];
+			}
+			return t;
+
+		}
