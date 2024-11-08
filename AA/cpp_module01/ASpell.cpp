92c4d06c371e1b139ad30e4dda4d1ae82ffa8f36
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-11-08 15:06:31 +0100

    by gacp

---
+++ AA/cpp_module01/ASpell.cpp
@@ -0,0 +1,37 @@
+#include "ASpell.hpp"
+
+
+
+ASpell::~ASpell()
+{
+
+}
+
+
+ASpell::ASpell(const ASpell& o)
+{
+	*this = o;
+}
+
+ASpell::ASpell(const std::string& name, const std::string& effects ) : _name (name), _effects(effects)
+{
+
+}
+
+
+
+ASpell& ASpell::operator=(const ASpell& o)
+{
+	_name = o._name;
+	_effects = o._effects;
+	return (*this);
+}
+
+const std::string& ASpell::getName() const
+{
+	return _name;
+}
+const std::string& ASpell::getEffects() const
+{
+	return _effects;
+}
