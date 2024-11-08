c3e77b5e778dd2db2a50a2ccefc77bd8a24bdb9e
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-11-08 14:50:30 +0100

    by gacp

--- AA/cpp_module00/Warlock.hpp
+++ AA/cpp_module00/Warlock.hpp
@@ -10,11 +10,11 @@ class Warlock
 	private:
 		std::string _name;
 		std::string _title;
+		Warlock(const Warlock& o);
+		Warlock& operator=(const Warlock& o);
 	public:
 		Warlock(const std::string& name, const std::string& title );
 		~Warlock();
-		Warlock(const Warlock& o);
-		Warlock& operator=(const Warlock& o);
 
 		const std::string& getName() const;
 		const std::string& getTitle() const;
