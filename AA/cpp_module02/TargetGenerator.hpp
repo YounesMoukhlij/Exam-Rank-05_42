0df5b759ee2555b6486e3878a75f05d5758ed693
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-11-08 16:02:28 +0100

    by gacp

---
+++ AA/cpp_module02/TargetGenerator.hpp
@@ -0,0 +1,30 @@
+
+#pragma once
+
+#include <iostream>
+#include "ASpell.hpp"
+#include "ATarget.hpp"
+#include <map>
+
+
+
+class SpellBook
+{
+	private:
+		SpellBook();
+		SpellBook(const SpellBook& origine);
+		SpellBook& operator=(const SpellBook& origine);
+		std::map<std::string, ATarget *> MAP;
+	public:
+		~SpellBook();
+
+
+		void	learnSpell(ASpell * spell);
+		void	forgetSpell(std::string& spell);
+		ASpell*	createSpell(std::string& spell);
+
+};
+
+
+
+
