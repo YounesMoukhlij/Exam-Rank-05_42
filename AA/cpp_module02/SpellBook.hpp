d7e55df5f90ddc8ae9816106083d68c8892e1124
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-11-08 15:56:38 +0100

    by gacp

--- AA/cpp_module02/SpellBook.hpp
+++ AA/cpp_module02/SpellBook.hpp
@@ -25,3 +25,78 @@ class SpellBook
 		void	createSpell(std::string& spell);
 
 };
+
+
+
+
+#include "Warlock.hpp"
+
+void				Warlock::setTitle(const std::string& title)
+{
+	_title = title;
+}
+
+Warlock::~Warlock()
+{
+	std::cout <<  _name << ": My job here is done!" << std::endl;
+}
+
+void	Warlock::introduce() const
+{
+	std::cout << _name << ": I am " << _name << ", " << _title << "!" << std::endl;
+}
+
+Warlock::Warlock(const Warlock& o)
+{
+	*this = o;
+}
+
+Warlock::Warlock(const std::string& name, const std::string& title ) : _name (name), _title(title)
+{
+	std::cout << _name << ": This looks like another boring day." << std::endl;
+}
+
+
+
+Warlock& Warlock::operator=(const Warlock& o)
+{
+	_name = o._name;
+	_title = o._title;
+	return (*this);
+}
+
+const std::string& Warlock::getName() const
+{
+	return _name;
+}
+const std::string& Warlock::getTitle() const
+{
+	return _title;
+}
+
+		void	Warlock::learnSpell(ASpell* spell)
+		{
+			if (spell)
+			{
+				if (MAP.find(spell->getName()) == MAP.end())
+					MAP[spell->getName()] = spell->clone();
+			}
+		}
+		void	Warlock::forgetSpell(std::string spell)
+		{
+				if (MAP.find(spell) != MAP.end())
+				{
+					delete MAP[spell];
+					MAP.erase(MAP.find(spell));
+				}
+
+
+		}
+		void	Warlock::launchSpell(std::string spell, ATarget& tar)
+		{
+				if (MAP.find(spell) != MAP.end())
+				{
+					MAP[spell]->launch(tar);
+				}
+
+		}
