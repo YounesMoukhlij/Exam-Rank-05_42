1ef29bdb57c7c2c140adda04eef47a2a0aea0a3b
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-11-08 15:21:13 +0100

    by gacp

---
+++ AA/cpp_module01/Dummy.hpp
@@ -0,0 +1,29 @@
+
+#pragma once
+
+
+#include "ASpell.hpp"
+
+class Fwoosh
+{
+	public:
+		Fwoosh();
+		 ~Fwoosh();
+		 ASpell*  clone () const;
+
+
+};
+
+Fwoosh::Fwoosh() : ASpell("Fwoosh", "fwooshed")
+{
+
+}
+		 Fwoosh::~Fwoosh()
+		 {
+
+		 }
+
+		 ASpell*  Fwoosh::clone () const
+		 {
+			return new Fwoosh();
+		 }
