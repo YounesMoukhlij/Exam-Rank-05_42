5419e2e35262b950ee34f9199d0fa31fb8b4ad58
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-11-12 11:36:40 +0100

    by gacp

---
+++ T/cpp_module01/ASpell.cpp
@@ -0,0 +1,31 @@
+#include "Warlock.hpp"
+
+		Warlock::~Warlock()
+		{
+			std::cout << _name << ": My job here is done!" << std::endl;
+		}
+
+		Warlock::Warlock(const Warlock& origine)
+		{
+			*this = origine;
+		}
+		Warlock::Warlock(const std::string& name, const std::string& title) : _name (name), _title(title)
+		{
+			std::cout << _name << ": This looks like another boring day." << std::endl;
+		}
+
+		Warlock& Warlock::operator=(const Warlock& origine)
+		{
+			_name = origine._name;
+			_title = origine._title;
+			return (*this);
+		}
+
+		const std::string& Warlock::getName() const
+		{
+			return _name;
+		}
+		const std::string& Warlock::getTitle() const
+		{
+			return _title;
+		}
