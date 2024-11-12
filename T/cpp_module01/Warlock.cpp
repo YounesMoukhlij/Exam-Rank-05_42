0e35c39743953b8f04a508333b03a54342a33011
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-11-12 11:59:40 +0100

    by gacp

--- T/cpp_module01/Warlock.cpp
+++ T/cpp_module01/Warlock.cpp
@@ -40,3 +40,30 @@
 		{
 			std::cout << _name << ": I am " << _name << ", " <<  _title << "!" << std::endl;
 		}
+
+
+		void Warlock::learnSpell(ASpell* spell)
+		{
+			if (spell)
+			{
+				if (MAP.find(spell->getName()) == MAP.end())
+				{
+					MAP[spell->getName()] = spell->clone();
+				}
+			}
+		}
+		void	Warlock::forgetSpell(std::string spell)
+		{
+			if (MAP.find(spell) != MAP.end())
+			{
+				delete MAP[spell];
+				MAP.erase(MAP.find(spell));
+			}
+		}
+		void	Warlock::launchSpell(std::string spell, ATarget& tar)
+		{
+			if (MAP.find(spell) != MAP.end())
+			{
+				MAP[spell]->launch(tar);
+			}
+		}
