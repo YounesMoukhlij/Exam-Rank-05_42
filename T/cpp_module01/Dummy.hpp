9c6adaf1ca05b8869fc4dc80abd59ab630ed6978
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-11-12 11:41:43 +0100

    by gacp

---
+++ T/cpp_module01/Dummy.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <iostream>
+
+
+class Fwoosh
+{
+	public:
+		Fwoosh();
+		~Fwoosh();
+		ASpell* clone() const;
+
+};
--- T/cpp_module01/Fwoosh.hpp
+++ T/cpp_module01/Fwoosh.hpp
@@ -8,6 +8,6 @@ class Fwoosh
 	public:
 		Fwoosh();
 		~Fwoosh();
-		ASpell* Fwoos
+		ASpell* clone() const;
 
 };
