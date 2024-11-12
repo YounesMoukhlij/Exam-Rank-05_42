a73094ba1980c5194810f192d5c9866a4ab3e004
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-11-12 12:54:37 +0100

    by gacp

--- T/cpp_module02/SpellBook.cpp
+++ T/cpp_module02/SpellBook.cpp
@@ -29,3 +29,24 @@
 				MAP[spell]->launch(tar);
 			}
 		}
+
+				Warlock::~Warlock()
+		{
+			std::cout << _name << ": My job here is done!" << std::endl;
+		}
+
+		Warlock::Warlock(const Warlock& origine)
+		{
+			*this = origine;
+		}
+		Warlock::Warlock(const std::string& name, const std::string& title) : _name (name), _title(title)
+		{
+			std::cout << _name << ": This looks like another boring day." << std::endl;
+		}
+
+		Warlock& Warlock::operator=(const Warlock& origine)
+		{
+			_name = origine._name;
+			_title = origine._title;
+			return (*this);
+		}
