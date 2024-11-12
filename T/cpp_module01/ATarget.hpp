543c259d5f4b23c4e92cd61fdf1264ffd4693170
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-11-12 11:39:13 +0100

    by gacp

--- T/cpp_module01/ATarget.hpp
+++ T/cpp_module01/ATarget.hpp
@@ -3,17 +3,17 @@
 #include <iostream>
 
 
-class ASpell
+class ATarget
 {
 	private:
 		std::string _name;
 		std::string _effects;
-		ASpell();
-		ASpell(const ASpell& origine);
-		ASpell& operator=(const ASpell& origine);
+		ATarget();
+		ATarget(const ATarget& origine);
+		ATarget& operator=(const ATarget& origine);
 	public:
-		~ASpell();
-		ASpell(const std::string& name, const std::string& effects);
+		~ATarget();
+		ATarget(const std::string& name, const std::string& effects);
 
 
 		const std::string& getName() const;
