42f5636a9acb73860166df2aa09b5cff740b63da
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-11-12 11:26:09 +0100

    by gacp

--- T/cpp_module00/Warlock.cpp
+++ T/cpp_module00/Warlock.cpp
@@ -36,7 +36,7 @@
 			_title = title;
 		}
 
-		void introduce() const
+		void Warlock::introduce() const
 		{
 			std::cout << _name << ": I am" << _name << ", " <<  _title << "!" << std::endl;
 		}
