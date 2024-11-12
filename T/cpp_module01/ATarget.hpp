994adee30e3e9a97b2ffdbe7f893b7403d876ac3
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-11-12 12:01:03 +0100

    by gacp

--- T/cpp_module01/ATarget.hpp
+++ T/cpp_module01/ATarget.hpp
@@ -12,10 +12,10 @@ class ATarget
 {
 	private:
 		std::string _type;
-		ATarget();
 		ATarget(const ATarget& origine);
 		ATarget& operator=(const ATarget& origine);
 	public:
+		ATarget();
 		~ATarget();
 		ATarget(const std::string& type);
 
