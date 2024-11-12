9cba71baae1d7b0cb53af29ef0a540b2687aa4ec
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-11-12 13:08:26 +0100

    by gacp

---
+++ T/cpp_module02/TargetGenerator.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <iostream>
+#include <map>
+#include "ASpell.hpp"
+#include "SpellBook.hpp"
+#include "ATarget.hpp"
+
+class ATarget;
+
+
+
+class TargetGenerator
+{
+	private:
+		TargetGenerator(const TargetGenerator& origine);
+		TargetGenerator& operator=(const TargetGenerator& origine);
+		std::map<std::string, ATarget*> MAP;
+	public:
+		TargetGenerator();
+		~TargetGenerator();
+
+
+
+		void learnTargetType(ATarget* tar);
+		void forgetTargetType(std::string const &tar);
+		ATarget* createTarget(std::string const &tar);
+};
